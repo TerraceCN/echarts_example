a2d9e67b8ce38ad4fca30485715823bf55a46410
TerraceCN <TerraceCN@users.noreply.github.com>
2022-05-04 15:56:14 +0000

    deploy: 9e6c2c6779f95c8b87fecd7305095c1721aa034b

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import ECharts from 'vue-echarts';
-import { use } from "echarts/core";
-import { CanvasRenderer } from "echarts/renderers";
-import App from './App.vue';
-
-use([CanvasRenderer]);
-
-createApp(App).component('v-chart', ECharts).mount('#app');
